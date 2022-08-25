d45368f892f071446e31f241246f887adb10652f
Gabriel Mendes <69258754+omgitsgm@users.noreply.github.com>
2022-08-25 15:57:14 -0300

    Add files via upload
    Exercício 2 do Laboratório 2 de SO.

---
+++ Lab02/ex2lab01.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(void){
+	
+	char nomeArquivoEntrada [100];
+	char nomeArquivoSaida [100];
+	
+	printf("Insira o nome do arquivo de entrada: ");
+	scanf("%s", nomeArquivoEntrada);
+	
+	printf("Insira o nome do arquivo de saida: ");
+	scanf("%s", nomeArquivoSaida);
+	
+	// Abrimos o arquivo de entrada
+	FILE *entrada = fopen(nomeArquivoEntrada, "r");
+	if(entrada == NULL){
+		printf("Arquivo nao encontrado.");
+		return -1;
+	}
+	
+	// Criamos um arquivo de saida, do tipo 'w', para poder receber o conteudo lido do arquivo de entrada
+	FILE *saida = fopen(nomeArquivoSaida, "w");
+	if(saida == NULL){
+		printf("Erro ao criar o arquivo");
+		return -1;
+	}
+	
+	while(true){
+		char linha[100];
+		
+		// Lemos cada linha do arquivo de entrada
+		int k = fscanf(entrada, "%s", &linha);
+		// Se nao ler nada, encerra o loop
+		if(k < 1)
+			break;
+			
+		// Imprimimos cada linha no arquivo de saida.
+		fprintf(saida, "%s\n", linha);
+		
+	}
+	printf("\n'%s' foi copiado em um novo arquivo chamado '%s'", nomeArquivoEntrada, nomeArquivoSaida);
+	
+	// Fechamos os dois arquivos
+	fclose(entrada);
+	fclose(saida);
+		
+	
+}
