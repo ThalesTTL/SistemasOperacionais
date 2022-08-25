40fd88f1e0f7d8bda036041964efcb30b9f9aafc
Gabriel Mendes <69258754+omgitsgm@users.noreply.github.com>
2022-08-25 17:09:34 -0300

    Update ex1lab01.c
    Exercício 1 corrigido. Agora ele consegue ler e imprimir linha a linha do programa e não apenas palavra por palavra como na primeira versão.

--- Lab02/ex1lab01.c
+++ Lab02/ex1lab01.c
@@ -3,7 +3,6 @@ Luiz Gabriel Profirio Mendes      TIA: 42082293
 Thales Torres Lopes               TIA: 32135513
 Victor Silva Fernandes            TIA: 32163967
 */ 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -11,29 +10,30 @@ Victor Silva Fernandes            TIA: 32163967
 int main(void){
 	
 	char nomeArquivo[100];
-	printf("Insira o nome do arquivo: ");
+	printf("Insira o nome do arquivo de entrada: ");
 	scanf("%s", nomeArquivo);
 	
 	// Abrimos o arquivo desejado passando o nome fornecido pelo usuario.
 	FILE *arquivo = fopen(nomeArquivo, "r");
 	
 	if(arquivo == NULL){ // Se fopen nos retornar null...
-		printf("O arquivo nao foi encontrado.");
+		printf("\nO arquivo nao foi encontrado.");
 		return -1;
 	} else { // Caso tenha um arquivo com o nome fornecido...
-		printf("Conteudo do arquivo '%s':\n", nomeArquivo);
-		printf("--[INICIO]--\n");
+		printf("\nConteudo do arquivo '%s':\n\n", nomeArquivo);
 		
 		while(true){
 			char linha[100]; // criamos um vetor de caracteres para armazenar o conteudo das linhas do arquivo
-			int k = fscanf(arquivo, "%s", &linha);
-			// fscanf sempre ira nos retornar o numero de objetos lidos, por isso, caso esse valor seja menor do que 1, significa que ele nao leu nada e chegou ao final do arquivo.
-			if(k < 1)
+			// efetuamos a leitura de linha a linha do arquivo
+			// caso a leitura chegue ate o final do arquivo, ele retorna um ponteiro NULL
+			char *ptr = fgets(linha, 100, arquivo);
+			// se o ponteiro apontar para NULL, encerra o while
+			if(ptr == NULL)
 				break;
-			printf("%s\n", linha); // imprimimos a linha
+			printf("%s", linha); // imprimimos a linha
 		}
 		
-		printf("--[FIM]--");
+		printf("\n\nFim da leitura do arquivo '%s'.", nomeArquivo);
 	}
 	
 	// Fechamos o arquivo que foi aberto
