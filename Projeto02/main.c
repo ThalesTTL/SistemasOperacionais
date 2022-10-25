5dd16623e04c5cd4027a288496d1072ab1135251
ThalesTTL <83013510+ThalesTTL@users.noreply.github.com>
2022-10-25 18:00:20 -0300

    update20221025 da main.c
    Tirei tudo de clone, usando só pthreads e por enquanto elas n acessam a função que deveriam realizar.

--- Projeto02/main.c
+++ Projeto02/main.c
@@ -7,6 +7,7 @@
 
   ------------------- References --------------------
   https://www.modernescpp.com/index.php/race-condition-versus-data-race
+  https://hpc-tutorials.llnl.gov/posix/
 */
 
 #define _GNU_SOURCE
@@ -15,9 +16,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 // 64kB stack
 #define FIBER_STACK 1024 * 64
+
+// defines 100 threads to work 
+#define NUM_THREADS 100
 
 int thread_count;
 
@@ -29,8 +34,8 @@ typedef struct c account;
 account from, to;
 int value, aux, count = 0;
 
-// The child thread will execute this function
-int transfer(void *arg) {
+// the thread will execute this function
+void *transfer(void *arg) {
   if (from.balance >= value) {
     from.balance -= value;
     to.balance += value;
@@ -39,18 +44,17 @@ int transfer(void *arg) {
   printf("Transfer done successfully!\n");
   printf("Money in c1: %d\n", from.balance);
   printf("Money in c2: %d\n", to.balance);
-  return 0;
 }
 
-// Exchanges 'to' and 'from'
-void exchange() {
+// exchanges 'to' and 'from'
+void exchange(c1,c2) {
   aux = from.balance;
   from.balance = to.balance;
   to.balance = aux;
   count += 1;
 }
 
-// Checks if order is correct
+// checks if order is correct
 bool check() {
   if ((count % 2) > 0) {
     return false;
@@ -58,45 +62,40 @@ bool check() {
     return true;
   }
 }
-//---------------//---------------//---------------//---------------//---------------//
+//---------------//---------------//---------------//---------------//---------------//---------------//
 int main(int argc, char* argv[]) {
   void *stack;
   pid_t pid;
-  int i;
-  long thread;
-  pthread_t* thread_handles;
-  // Get number of threads from command line 
+  int i, rc;
+  long t;
+  //pthread_t* thread_handles; 
+  pthread_t threads[NUM_THREADS];
+  
+  /*
+  Get number of threads from command line 
   thread_count = strtol(argv[1], NULL, 10);      
   thread_handles = malloc(thread_count*sizeof(pthread_t));
+  */
   
-  // Allocate the stack
-  //stack = malloc(FIBER_STACK);
-
   srand(time(NULL)); // inicializes the seed for rand as NULL for different results in different runs
 
-  if (stack == 0) {
-    perror("malloc: could not allocate stack");
-    exit(1);
-  }
-  // All accounts start with 100 dollars
+  // all accounts start with 100 currency
   from.balance = 100;
   to.balance = 100;
   
-  value = rand() % 20; // Aleatory number between 0 and 20 that signifies the transfer value 
-  printf("Transfering %d to account c2\n", value);  
+  value = rand() % 20; // aleatory number between 0 and 20 that signifies the transfer value 
+  printf("Transfering %d\n", value); 
 
-  /*for (i = 0; i < 100; i++) {
-    // Call the clone system call to create the child thread
-    pid = clone(&transfer, (char *)stack + FIBER_STACK,
-                SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0);
-    if (pid == -1) {
-      perror("clone");
-      exit(2);
+  for (t = 0; t<thread_count; t++){
+    rc = pthread_create(&threads[t], NULL, transfer, (void *)t);
+    if(rc){
+      printf("ERROR: return code from pthread_create() is %d\n", rc);
+      exit(-1);
     }
   }
-  */ 
-  // Free the stack
-  //printf("Transfers done and memory freed.\n");
-  //free(stack);
+
+  // free the malloc
+  printf("Transfers done and memory freed.\n");
+  pthread_exit(NULL);
   return 0;
 }
