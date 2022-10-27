d6806acd2420e1e695675565ccb29c32b4408b53
ThalesTTL <83013510+ThalesTTL@users.noreply.github.com>
2022-10-27 18:50:58 -0300

    update20221026 da main.c
    Os cálculos tão certos, só tão estão muito rápidos. Não aguento mais família

--- Projeto02/main.c
+++ Projeto02/main.c
@@ -8,6 +8,7 @@
   ------------------- References --------------------
   https://www.modernescpp.com/index.php/race-condition-versus-data-race
   https://hpc-tutorials.llnl.gov/posix/
+  https://alvinalexander.com/programming/printf-format-cheat-sheet/
 */
 
 //---------------//---------------//---------------//---------------//---------------//---------------//
@@ -22,13 +23,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-// 64kB stack
-#define FIBER_STACK 1024 * 64
-
 // defines 100 threads to work 
-#define NUM_THREADS 5
-
-int thread_count;
+#define NUM_THREADS 2
 
 struct c {
   int balance;
@@ -43,32 +39,59 @@ long tid[NUM_THREADS];
 // Functions that are used in the main.                                                               //
 //---------------//---------------//---------------//---------------//---------------//---------------//
 
-// function for printing the balance in both accounts
-void print_balance(){
+// function for printing the balance in both accounts when transfer succeds
+void print_balance1(){
   printf("|--------------------------------------|\n");
   printf("|Transfer done successfully!           |\n");
   printf("|Balance in c1: %-5d                  |\n", from.balance);
   printf("|Balance in c2: %-5d                  |\n", to.balance);
+  printf("|--------------------------------------|\n\n\n");
+  printf("Transfering %d...\n", value);
+  }
+
+// function for printing the balance in both accounts when transfer fails
+void print_balance2(){
   printf("|--------------------------------------|\n");
+  printf("|Transfer failed!                      |\n");
+  printf("|Balance in c1: %-5d                  |\n", from.balance);
+  printf("|Balance in c2: %-5d                  |\n", to.balance);
+  printf("|--------------------------------------|\n\n\n");
+  printf("Transfering %d...\n", value);
   }
-  
-// the threads will execute this function
-void *transfer(void *arg) {
-  if (from.balance >= value) {
-    from.balance -= value;
-    to.balance += value;
-    value = rand() % 20;
-  }
-  print_balance();
-  return 0;
-}
   
 // exchanges 'to' and 'from'
-void exchange() {
-  aux = from.balance;
-  from.balance = to.balance;
-  to.balance = aux;
-  count += 1;
+void *transfer1(void *arg) {
+  while(1){
+    if (from.balance >= value) {
+      from.balance -= value;
+      to.balance += value;
+        
+      value = (rand() % 19)+ 1; // recalculates the value from 1 to 20
+      print_balance1();
+    } 
+    else {
+      value = (rand() % 19)+ 1; // recalculates the value from 1 to 20
+      print_balance2();
+    }
+  }
+  return 0;
+}
+
+void *transfer2(void *arg) {
+  while(1){
+    if (to.balance >= value) {
+      to.balance -= value;
+      from.balance += value;
+        
+      value = (rand() % 19)+ 1; // recalculates the value from 1 to 20
+      print_balance1();
+    } 
+    else {
+      value = (rand() % 19)+ 1; // recalculates the value from 1 to 20
+      print_balance2();
+    }
+  }
+  return 0;
 }
 
 // checks if order is correct
@@ -85,9 +108,9 @@ bool check() {
 //---------------//---------------//---------------//---------------//---------------//---------------//
 
 int main(int argc, char* argv[]) {
-  void *stack;
   pid_t pid;
-  int i, rc;
+  int i, rc, value;
+  void *status;
   long t;
   pthread_t threads[NUM_THREADS];
   
@@ -97,18 +120,23 @@ int main(int argc, char* argv[]) {
   from.balance = 100;
   to.balance = 100;
 
-
-  
-  value = rand() % 20; // aleatory number between 0 and 20 that signifies the transfer value 
-  printf("Transfering %d...\n", value); 
-  for (t = 0; t<thread_count; t++){
+  value = (rand() % 20) + 1; // aleatory number between 1 and 20 that signifies the transfer value  
+  printf("Transfering %d...\n", value);
+  for (t = 0; t < NUM_THREADS; t++){
     tid[t] = t;
-    rc = pthread_create(&threads[t], NULL, transfer, (void *) tid[t]);
+    if (t % 2 > 0){ // divide the threads 50/50
+      pthread_create(&threads[t], NULL, transfer1, (void *) tid[t]);
+    }
+    else {
+      pthread_create(&threads[t], NULL, transfer2, (void *) tid[t]);
+    }
+    
+    pthread_join(threads[t], &status);  
+    }
     if(rc){
       printf("ERROR: return code from pthread_create() is %d\n", rc);
       exit(-1);
     }
-  }
   // stop the threads
   printf("Transfers done and memory freed!\n");
   pthread_exit(NULL);
